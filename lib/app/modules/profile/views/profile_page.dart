664beecc12af5a8a3b0d6ae6170407f1b56e5743
arjunpspadiyoor10@gmail.com <arjunpshaji@techfriar.com>
2025-04-15 23:57:42 +0530

    modified profile section

--- lib/app/modules/profile/views/profile_page.dart
+++ lib/app/modules/profile/views/profile_page.dart
@@ -6,6 +6,7 @@ import 'package:logiology/app/modules/profile/controllers/profile_controller.dar
 import 'package:logiology/theme/app_theme.dart';
 import 'package:logiology/theme/widgets/common_appbar.dart';
 import 'package:logiology/theme/widgets/input_text.dart';
+import 'package:logiology/theme/widgets/primary_button.dart';
 
 class ProfilePage extends StatelessWidget {
   final controller = Get.put(ProfileController());
@@ -22,19 +23,35 @@ class ProfilePage extends StatelessWidget {
       body: Obx(() {
         usernameCtrl.text = controller.username.value;
         passwordCtrl.text = controller.password.value;
-        return Column(
-          children: [
-            GestureDetector(
-              onTap: controller.pickImage,
-              child:
-                  controller.imagePath.isEmpty
-                      ? Icon(Icons.person, size: 100, color: appColor(context).primaryText)
-                      : Image.file(File(controller.imagePath.value), height: 100),
-            ),
-            InputText(onChanged: (x) {}, labelText: "Username", controller: usernameCtrl),
-            InputText(onChanged: (x) {}, labelText: "Password", controller: passwordCtrl, obscureText: true),
-            ElevatedButton(onPressed: () => controller.updateProfile(usernameCtrl.text, passwordCtrl.text), child: Text("Save")),
-          ],
+        return Padding(
+          padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: controller.pickImage,
+                child:
+                    controller.imagePath.isEmpty
+                        ? Icon(Icons.person, size: 100, color: appColor(context).primaryText)
+                        : Image.file(File(controller.imagePath.value), height: 100),
+              ),
+              SizedBox(height: 32),
+              InputText(onChanged: (x) {}, labelText: "Username", controller: usernameCtrl),
+              SizedBox(height: 16),
+              InputText(onChanged: (x) {}, labelText: "Password", controller: passwordCtrl, obscureText: true),
+              SizedBox(height: 24),
+              PrimaryButton(
+                buttonText: "Save",
+                onPressed: () => controller.updateProfile(usernameCtrl.text, passwordCtrl.text),
+                buttonColor: appColor(context).buttonColor,
+              ),
+              SizedBox(height: 16),
+              PrimaryButton(
+                buttonText: "Logout",
+                onPressed: () => Get.toNamed("/"),
+                buttonColor: appColor(context).errorText,
+              ),
+            ],
+          ),
         );
       }),
     );
